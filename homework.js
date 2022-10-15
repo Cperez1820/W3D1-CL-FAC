22ccbae9ce4e1c355d81b2a03f94ed9aa18f54b5
chris p <christopherperez1820@gmail.com>
2022-10-14 20:26:48 -0400

    created hamster class

---
+++ homework.js
@@ -0,0 +1,37 @@
+//================================= Classes and factories =========================================================
+
+class Hamster{
+    constructor(ownerParam, nameParam, priceParam) {
+        this.owner = "";
+        this.name = nameParam;
+        this.price = 15;
+            wheelRun();{
+                this.wheelRun = true;
+                    console.log(this.owner + "Squeak Squeak")
+            }
+            eatFood(); {
+                this.eatFood = true;
+                    console.log(this.owner + "nibble nibble")
+            }
+            getPrice();{
+                this.getPrice = true;
+                    return this.price
+            }
+        
+    }
+    
+}
+
+
+
+
+
+
+
+//intial thought 
+// function hamster() {
+//     owner:"",
+//     name:"",
+//     price: "",
+
+// }
